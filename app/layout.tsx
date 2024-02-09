748c7b556d99532d440fd4ae3144de7be2d21f20
jad-delgadillo <jad-delgadillo@github.com>
2024-02-09 00:56:08 -0600

    added metadata on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,28 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import SocialNavBar from "./components/SocialNavBar";
+
+export const metadata: Metadata = {
+  title: "Jorge Delgadillo",
+  description: "Dangerously good creativity",
+  metadataBase: new URL("https://about.alwaysjad.dev"),
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Alwaysjad",
+    description: "Dangerously good creativity",
+    url: "https://about.alwaysjad.dev",
+    siteName: "Alwaysjad",
+    images: {
+      url: "https://res.cloudinary.com/dzepeibjw/image/upload/v1659152027/Metadata-image---alw_nrcnx6.png",
+      width: 1820,
+      height: 904,
+      alt: "Website link image banner",
+    },
+  },
+};
 
 export default function RootLayout({
   children,
