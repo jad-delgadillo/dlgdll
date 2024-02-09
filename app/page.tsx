f686974ef6ec08aac96e476d438a77211c737ecc
jad-delgadillo <jad-delgadillo@github.com>
2024-02-09 01:16:08 -0600

    improved hero section for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="">
       <div className="flex flex-col items-center justify-center bg-white text-black ">
-        <div className="flex h-screen w-screen flex-col items-center justify-between overflow-auto p-3 md:space-y-40">
+        <div className="flex w-screen flex-col items-center overflow-auto justify-between p-3 md:space-y-40">
           <div className="mt-9 w-full">
             <div className=" w-7/12 md:w-2/12">
               <ImageBlur src={me} />
@@ -20,7 +20,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className=" pb-20 md:pb-14 ">
+          <div className="py-20 md:pb-14 ">
             <p className="text-2xl font-medium leading-none lg:w-5/6 lg:text-[3rem]">
               {" "}
               Hey, I&apos;m <span className="text-[#FF480F]"> Jorge</span>. A
