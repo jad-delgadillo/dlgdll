cd30b294a753487ca0ff346b6213ab47c8eee237
jad-delgadillo <jad-delgadillo@github.com>
2024-03-09 17:21:20 -0600

    all the media is now being hosted in cloudinary, in this case the one from hello page

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import me3 from "../../public/Photos/me11.jpg";
+
 import LinkButtons from "../components/LinkButtons";
 
 export default function Hello() {
@@ -12,12 +12,11 @@ export default function Hello() {
         </div>
         <div className="w-full h-[22rem] relative ">
           <Image
-            src={me3}
+            src="https://res.cloudinary.com/dzepeibjw/image/upload/v1710023341/Aldem-01/photos/eticc4vfd7pnrq6pm6df.jpg"
             layout="fill"
             objectFit="cover"
             alt="me"
             className="rounded-lg"
-            placeholder="blur"
           />
         </div>
         <div className="bg-white font-medium text-lg rounded-lg px-3 py-4">
