f73b5906bb3cb93ce9ab090c06e79efbe6daa5ae
jad-delgadillo <jad-delgadillo@github.com>
2024-04-18 12:06:52 -0600

    Projects Page | Improved overlay div for when the video is playing

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import projectData from "../../utils/data";
 import Link from "next/link";
@@ -10,16 +10,37 @@ const ProjectPage = () => {
   const currentProject = projectData.find((p) => p.id === projectId);
 
   const [isPlaying, setIsPlaying] = useState(false);
+  const [overlayZIndex, setOverlayZIndex] = useState(-1);
+  const [buttonZIndex, setButtonZIndex] = useState(50); // Initial lower zIndex when video is potentially playing
+
+  const videoElement = useRef(null);
 
   if (!currentProject) {
     return <div>Project not found</div>;
   }
 
-  const handlePlay = () => setIsPlaying(true);
-  const handlePause = () => setIsPlaying(false);
+  const handlePlaying = () => {
+    setIsPlaying(true);
+    setOverlayZIndex(60);
+    setButtonZIndex(50); // Lower zIndex while video is playing
+  };
 
-  // You can adjust the opacity level here
-  const desiredOpacity = 0.6; // Change this value to control the dim level
+  const handlePause = () => {
+    setIsPlaying(false);
+    setTimeout(() => {
+      setOverlayZIndex(-1);
+      setButtonZIndex(100); // Raise zIndex after animation completes
+    }, 500); // Ensure this matches the duration of the fade-out animation
+  };
+
+  const handleSeeking = () => {
+    if (
+      videoElement.current &&
+      !(videoElement.current as HTMLVideoElement).paused
+    ) {
+      setIsPlaying(true); // Maintain the overlay if the video is expected to continue playing
+    }
+  };
 
   return (
     <main>
@@ -27,13 +48,16 @@ const ProjectPage = () => {
         <div
           className="absolute inset-0 bg-black transition-opacity duration-500 ease-in-out"
           style={{
-            opacity: isPlaying ? desiredOpacity : 0, // Use the variable here
-            zIndex: isPlaying ? 60 : -1, // Higher than navbar to ensure it covers everything
+            opacity: isPlaying ? 0.6 : 0,
+            zIndex: overlayZIndex,
           }}
-        ></div>{" "}
-        {/* Animated overlay */}
+        ></div>
+
         <div className="flex flex-col items-center justify-center md:max-w-7xl md:flex-row md:items-center md:space-x-10 ">
-          <div className="flex w-screen flex-col items-start justify-center p-3 md:w-1/2">
+          <div
+            className="flex w-screen flex-col items-start justify-center p-3 md:w-1/2 "
+            style={{ zIndex: buttonZIndex }}
+          >
             <h1 className="text-6xl">{currentProject.title}</h1>
             <h2 className="text-4xl">{currentProject.company}</h2>
             <p className="text-xl font-medium">{currentProject.description}</p>
@@ -47,12 +71,18 @@ const ProjectPage = () => {
               className="rounded-lg z-[60] bg-black "
               src={currentProject.videoUrl}
               controls
-              onPlay={handlePlay}
+              ref={videoElement}
+              onPlay={handlePlaying}
               onPause={handlePause}
+              onSeeking={handleSeeking}
             />
           </div>
         </div>
-        <div className="absolute bottom-20 left-0 right-0 flex w-full justify-center">
+
+        <div
+          className="absolute bottom-14 md:bottom-20 left-0 right-0 flex w-full justify-center"
+          style={{ zIndex: buttonZIndex }}
+        >
           <Link href={"/"}>
             <button className="rounded-xl border p-3 text-neutral-400 transition-all hover:border-black hover:text-black">
               Home
